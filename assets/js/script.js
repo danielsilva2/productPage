0d00f0f7e7b3b9b59b04ef01b179515ccbdcee5c
danielsilva2 <daniel.silva0740@gmail.com>
2021-08-07 17:40:13 -0300

    1.0

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+
+const imgSecond = document.querySelector('.content-right img:last-child');
+
+imgSecond.addEventListener('mouseover', () => {
+  document.querySelector('.content-right img:first:child').style.opacity = 0;
+});
+imgSecond.addEventListener('mouseleave', () => {
+  document.querySelector('.content-right img:first:child').style.opacity = 1;
+});
